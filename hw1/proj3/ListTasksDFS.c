52fad32766bf69f692e224e184cdfcc6eba5479f
Xanonymous <trusaidlin@gmail.com>
2022-03-28 00:07:11 +0800

    style: reformat project files

--- hw1/proj3/ListTasksDFS.c
+++ hw1/proj3/ListTasksDFS.c
@@ -1,36 +1,36 @@
-#include <linux/sched/task.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/sched/task.h>
 
 void dfs(struct task_struct *task) {
-	struct task_struct *child; // pointer to the next child
-	struct list_head *list; // children
-	
-	printk(KERN_INFO "Name: %-20s State: %ld\tProcess ID: %d\n", task->comm, task->state, task->pid);
-	
-	list_for_each(list, &task->children) { // iterate over children of init_list
-		child = list_entry(list, struct task_struct, sibling); // get child
-		/* child points to the next child in the list */
-		dfs(child);
-	}
+    struct task_struct *child; // pointer to the next child
+    struct list_head *list;    // children
+
+    printk(KERN_INFO "Name: %-20s State: %ld\tProcess ID: %d\n", task->comm, task->state, task->pid);
+
+    list_for_each(list, &task->children) {                     // iterate over children of init_list
+        child = list_entry(list, struct task_struct, sibling); // get child
+        /* child points to the next child in the list */
+        dfs(child);
+    }
 }
 
 /* This function is called when the module is loaded. */
 int entryPoint(void) {
-	printk(KERN_INFO "Inserting ListTasksDFS\n");	
-	
-	printk(KERN_INFO "Started listing tasks\n");
-	
-	dfs(&init_task); // DFS start from init_task
-	
-	printk(KERN_INFO "Stopped listing tasks\n");
-	return 0;
+    printk(KERN_INFO "Inserting ListTasksDFS\n");
+
+    printk(KERN_INFO "Started listing tasks\n");
+
+    dfs(&init_task); // DFS start from init_task
+
+    printk(KERN_INFO "Stopped listing tasks\n");
+    return 0;
 }
 
 /* This function is called when the module is removed. */
 void exitPoint(void) {
-	printk(KERN_INFO "Removing ListTasksDFS\n");
+    printk(KERN_INFO "Removing ListTasksDFS\n");
 }
 
 /* Macros for registering module entry and exit points. */
--- hw1/proj3/ListTasksLinear.c
+++ hw1/proj3/ListTasksLinear.c
@@ -1,32 +1,32 @@
-#include <linux/sched/signal.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/sched/signal.h>
 
 /* This function is called when the module is loaded (insmod command). */
 int entryPoint(void) {
-	struct task_struct *task;
+    struct task_struct *task;
 
-	printk(KERN_INFO "Inserting ListTasksLinear\n");	
-	printk(KERN_INFO "Started listing tasks\n");
+    printk(KERN_INFO "Inserting ListTasksLinear\n");
+    printk(KERN_INFO "Started listing tasks\n");
 
-	for_each_process(task) { // iterate over all current tasks in the system
+    for_each_process(task) { // iterate over all current tasks in the system
 
-		/* task points to the next task on each iteration */
+        /* task points to the next task on each iteration */
 
-		printk(KERN_INFO "Name: %-20s State: %ld\tProcess ID: %d\n", 
-				task->comm, // task' name
-				task->state, // task's state (-1 unrunnable, 0 runnable, >0 stopped)
-				task->pid); // task's process ID
-	}
-	
-	printk(KERN_INFO "Stopped listing tasks\n");
-	return 0;
+        printk(KERN_INFO "Name: %-20s State: %ld\tProcess ID: %d\n",
+               task->comm,  // task' name
+               task->state, // task's state (-1 unrunnable, 0 runnable, >0 stopped)
+               task->pid);  // task's process ID
+    }
+
+    printk(KERN_INFO "Stopped listing tasks\n");
+    return 0;
 }
 
 /* This function is called when the module is removed (rmmod command). */
 void exitPoint(void) {
-	printk(KERN_INFO "Removing ListTasksLinear\n");
+    printk(KERN_INFO "Removing ListTasksLinear\n");
 }
 
 /* Macros for registering module entry and exit points. */
