bf4075c5716d2a46a90ff4261dbfd6689de817fb
Xanonymous <trusaidlin@gmail.com>
2022-03-28 01:17:11 +0800

    comment: add code comment for part1

--- hw1/proj1/part1/apple.c
+++ hw1/proj1/part1/apple.c
@@ -9,20 +9,48 @@
  *
  */
 
-#include <linux/init.h>
 #include <linux/module.h>
 
 MODULE_DESCRIPTION("A simple kernel module to test the kernel module load/unload process.");
 MODULE_LICENSE("GPL");
 
+/**
+ * @brief The callback function of kernel module load event.
+ * It will be called and write a msg to kernel ring buffer after this module has already loaded.
+ * @return int: indicates the module load event callback is executed successfully.
+ */
 int hello_init(void) {
+    // Write a simple msg into kernel ring buffer.
+    //
+    // `KERN_INFO` is a category code that identifies the category of this message.
+    //
+    // There are many other categories such as:
+    // KERN_EMERG   -> system is unusable
+    // KERN_ALERT   -> action must be taken immediately
+    // KERN_CRIT    -> critical conditions
+    // KERN_ERR     -> error conditions
+    // KERN_WARNING -> warning conditions
+    // KERN_NOTICE  -> normal but significant condition
+    // KERN_INFO    -> informational
+    // KERN_DEBUG   -> debug-level messages
+    //
+    // Please refer to this file for more details:
+    // https://github.com/torvalds/linux/blob/5bfc75d92efd494db37f5c4c173d3639d4772966/include/linux/kern_levels.h
     printk(KERN_INFO "Hello world !\n");
     return 0;
 }
 
+/**
+ * @brief The callback function of kernel module unload event.
+ * It will be called and write a msg to kernel ring buffer after this module has already unloaded.
+ */
 void hello_exit(void) {
+    // Write a simple msg into kernel ring buffer.
     printk(KERN_INFO "Bye !\n");
 }
 
+// Register the module callback functions on module (did) load event.
 module_init(hello_init);
+
+// Register the module callback functions on module (did) unload event.
 module_exit(hello_exit);
