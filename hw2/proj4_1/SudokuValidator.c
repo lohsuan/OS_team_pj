b32974334ca978b49ac9e3bce93e8f5ced481599
tlkuo <tzulin0517@gmail.com>
2022-04-20 16:38:23 +0800

    updated SudokuValidator.c, can validate valid or invalid sudoku solution while execution

--- hw2/proj4_1/SudokuValidator.c
+++ hw2/proj4_1/SudokuValidator.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
 #define num_threads 27
 
 /* 
@@ -18,7 +19,8 @@ typedef struct {
 } parameters;
 
 // Sudoku puzzle to be solved
-int sudoku[9][9] = {
+int sudoku[9][9] = {0};
+int sudoku_valid[9][9] = {
 	{6, 2, 4, 5, 3, 9, 1, 8, 7},
 	{5, 1, 9, 7, 2, 8, 6, 3, 4},
 	{8, 3, 7, 6, 1, 4, 2, 9, 5},
@@ -29,8 +31,18 @@ int sudoku[9][9] = {
 	{4, 9, 6, 1, 8, 2, 5, 7, 3},
 	{2, 8, 5, 4, 7, 3, 9, 1, 6}
 };
-/* 
-// other valid sudoku puzzles
+int sudoku_invalid[9][9] = {
+	{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{1, 2, 3, 4, 5, 6, 7, 8, 9}
+};
+/* other valid sudoku puzzles
 {
 	{8, 7, 6, 4, 9, 3, 2, 5, 1},
 	{3, 4, 5, 7, 1, 2, 9, 6, 8},
@@ -52,31 +64,7 @@ int sudoku[9][9] = {
 	{5, 2, 7, 4, 1, 8, 9, 3, 6},
 	{6, 4, 8, 9, 3, 7, 5, 2, 1},
 	{1, 9, 3, 2, 6, 5, 7, 4, 8}
-}
-// invalid sudoku puzzles
-{
-	{1, 2, 3, 4, 5, 6, 7, 8, 9},
-	{1, 2, 3, 4, 5, 6, 7, 8, 9},
-	{1, 2, 3, 4, 5, 6, 7, 8, 9},
-	{1, 2, 3, 4, 5, 6, 7, 8, 9},
-	{1, 2, 3, 4, 5, 6, 7, 8, 9},
-	{1, 2, 3, 4, 5, 6, 7, 8, 9},
-	{1, 2, 3, 4, 5, 6, 7, 8, 9},
-	{1, 2, 3, 4, 5, 6, 7, 8, 9},
-	{1, 2, 3, 4, 5, 6, 7, 8, 9}
-},
-{
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1}
-}
-*/
+} */
 
 // Method that determines if numbers 1-9 only appear once in a column
 void *isColumnValid(void* param) {
@@ -161,7 +149,30 @@ void *is3x3Valid(void* param) {
 	pthread_exit(NULL);
 }
 
-int main() {	
+int main(int argc, char *argv[]) {	
+	if (argc < 2) {
+		printf("Usage: executable-name [valid/invalid]\n");
+		return EXIT_FAILURE;
+	}
+	if (strcmp(argv[1], "valid") == 0) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				sudoku[i][j] = sudoku_valid[i][j];
+			}
+		}
+	}
+	else if(strcmp(argv[1], "invalid") == 0) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				sudoku[i][j] = sudoku_invalid[i][j];
+			}
+		}
+	}
+	else {
+		printf("Usage: executable-name [valid/invalid]\n");
+		return EXIT_FAILURE;
+	}
+
 	pthread_t threads[num_threads];
 	
 	int threadIndex = 0;	
--- hw2/proj4_1/readme.md
+++ hw2/proj4_1/readme.md
@@ -8,5 +8,6 @@
 ### How to Run
 1. open terminal in this directory
 2. enter `make` to compile
-3. enter `make exec` or `./main` to execute
-4. enter `make clean` to clean up (optional)
+3. enter `make valid` or `./main valid` to validate a valid sudoku solution
+4. enter `make invalid` or `./main invalid` to validate an invalid sudoku solution
+5. enter `make clean` to clean up (optional)
